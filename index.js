3c262a08d864542e718970b1a94fc5e6b4bf38e3
Regina Wanjiru Kariuki <wanjiruregina@gmail.com>
2025-03-20 20:32:43 +0300

    DOM Content Loaded

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function(){
+    const texts = document.getElementById("text");
+    texts.textContent = "This is really cool!";
+});
